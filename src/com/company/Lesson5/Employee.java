d59f46f53640dcbb4119e7421c1a0f7635d19bc5
Olya <kandikowa.ol@yandex.ru>
2020-11-16 20:14:25 +0300

    Проверка видимости Employee в lesson7 с помощью вызова из Lesson5

--- src/com/company/Lesson5/Employee.java
+++ src/com/company/Lesson5/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
     double salary;
     String department;
 
-    Employee(int id, String surname, int age, double salary, String department) {
+    public Employee(int id, String surname, int age, double salary, String department) {
         this.id = id;
         this.surname = surname;
         this.age = age;
--- src/com/company/Lesson7/Employee.java
+++ src/com/company/Lesson7/Employee.java
@@ -1,12 +1,24 @@
 package com.company.Lesson7;
 
 public class Employee {
-    double salary;
-    void dvoynayaZP(){
-        System.out.println("Dvoynaya z/p= "+ salary*2);
+    public double salary;
+
+    public void dvoynayaZP() {
+        System.out.println("Dvoynaya z/p= " + salary * 2);
 
     }
-    Employee(double salary2) {
+
+    public Employee(double salary2) {
+        salary = salary2;
 
     }
+
+    public static void main(String[] args) {
+        Employee emp = new Employee(500);
+        System.out.println(emp.salary);
+        emp.dvoynayaZP();
+    }
+
+
+
 }
