cc43d6bd19a19f0182aa82578e32d4661eeac565
Olya <kandikowa.ol@yandex.ru>
2020-11-16 21:02:11 +0300

    Проверка видимости Employee в другом пекедже

---
+++ src/com/company/Lesson7_1/TretiyClass.java
@@ -0,0 +1,12 @@
+package com.company.Lesson7_1;
+
+
+public class TretiyClass {
+
+    public static void main(String[] args) {
+       com.company. Lesson7.Employee emp = new com.company.Lesson7.Employee(500);
+        System.out.println(emp.salary);
+        emp.dvoynayaZP();
+
+    }
+}
