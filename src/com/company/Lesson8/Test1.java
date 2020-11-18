eaa5235db9fa5e126b8cbe718f8a4d5e8fbddd4e
Olya <kandikowa.ol@yandex.ru>
2020-11-18 21:03:57 +0300

    константа в конструкторе

--- src/com/company/Lesson8/Test1.java
+++ src/com/company/Lesson8/Test1.java
@@ -1,11 +1,18 @@
 package com.company.Lesson8;
 
 public class Test1 {
-    public  int a;
+    public final int a;
+
+    Test1(){
+        a=10;
+    }
+
+    Test1(boolean b){
+        a=10;
+    }
 
     public static void main(String[] args) {
         Test1 t = new Test1();
         System.out.println(t.a);
-
     }
 }
