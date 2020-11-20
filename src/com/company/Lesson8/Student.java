42f8d0855d8e9b9c92d651d6bd7c5f6b50c988c9
Olya <kandikowa.ol@yandex.ru>
2020-11-20 16:02:55 +0300

    static

--- src/com/company/Lesson8/Student.java
+++ src/com/company/Lesson8/Student.java
@@ -4,33 +4,40 @@ public class Student {
     String name;
     int course;
     static int count;
-    public Student(String name2,int course2){
+    int a;
+
+    public Student(String name2, int course2) {
         count++;
         name = name2;
         course = course2;
-        System.out.println("Student # " + count +" sozdan");
+        System.out.println("Student # " + count + " sozdan");
 
     }
 
-    public static void showCount(){
-        System.out.println("Vsego sozdanno studentov "+ count);
+    public static void showCount() {
+        System.out.println("Vsego sozdanno studentov " + count);
 
     }
 
+    public void showInfo() {
+        System.out.println("Welcome to the Student class !!!");
+    }
+
+    void abc() {
+        a++;
+        count++;
+    }
+
+    static void abcd() {
+        count++;
+        Student st2 = new Student("Petr", 1);
+        st2.a++;
+    }
 
 
     public static void main(String[] args) {
-        Student st1 = new Student("Ivan",1);
-        Student st2 = new Student("Petr",4);
-        Student st3 = new Student("Masha",2);
-
-        System.out.println(st1.name);
-        System.out.println(Student.count);
-        showCount();
-        Student.showCount();
-        st2.showCount();
-
-
-
+        abcd();
+        Student st3 = new Student("Ivan", 4);
+        st3.abc();
     }
 }
