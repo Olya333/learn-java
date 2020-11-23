175c3c9379a2a12b9e6d34b19a880a952d4e733c
Olya <kandikowa.ol@yandex.ru>
2020-11-23 19:53:35 +0300

    java.lang.String - адрес стринг по умолчанию

--- src/com/company/Lesson10/A.java
+++ src/com/company/Lesson10/A.java
@@ -1,11 +1,19 @@
 package com.company.Lesson10;
+
 import com.company.Lesson9.*;
-import com.company.Lesson8.*;
+
 public class A {
     public static void main(String[] args) {
 
-        com.company.Lesson8.Car c2 = new com.company.Lesson8.Car();
-        com.company.Lesson9.Student st2 = new com.company.Lesson9.Student();
+        java.lang.String st1 = new java.lang.String("hello");
+        Student st2 = new Student();
+        System.out.println(st2.z);
+        System.out.println(Student.c);
 
     }
+
+}
+
+class B {
+    Student st3 = new Student();
 }
