da6c10661a8cd483c6e62cc3f630ad1030d42153
Olya <kandikowa.ol@yandex.ru>
2020-11-27 16:20:36 +0300

    if(boolean expression)         {our code} else if(boolean expression)         {our code} else         {our code}

--- src/com/company/Lesson12/Test10.java
+++ src/com/company/Lesson12/Test10.java
@@ -2,16 +2,16 @@ package com.company.Lesson12;
 
 public class Test10 {
     public static void main(String[] args) {
-        int salary = 600;
+        int salary = 300;
 
-        if (salary>=500){
-            System.out.println(":)");
-        }
-
-        else {
-            System.out.println(":(");
-
-
+        if (salary < 200) {
+            System.out.println("Z/p ochen nizka");
+        } else if (salary < 400) {
+            System.out.println("Z/p srednego razmera");
+        } else if (salary < 600) {
+            System.out.println("Z/p visokaya");
+        } else {
+            System.out.println("Z/p otlichnaya");
         }
     }
 }
