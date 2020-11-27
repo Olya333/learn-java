586db9efc002ebd783483ea38e223677f7bf2fd5
Olya <kandikowa.ol@yandex.ru>
2020-11-27 13:55:51 +0300

    пример: if(boolean expression)             {our code}         else             {our code}

---
+++ src/com/company/Lesson12/Test10.java
@@ -0,0 +1,17 @@
+package com.company.Lesson12;
+
+public class Test10 {
+    public static void main(String[] args) {
+        int salary = 600;
+
+        if (salary>=500){
+            System.out.println(":)");
+        }
+
+        else {
+            System.out.println(":(");
+
+
+        }
+    }
+}
