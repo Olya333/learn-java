8648606112a2fb62e318097d265ed82946e5735f
Olya <kandikowa.ol@yandex.ru>
2020-11-30 10:53:51 +0300

    Пример ternary operator

--- src/com/company/Lesson12/Test10.java
+++ src/com/company/Lesson12/Test10.java
@@ -3,24 +3,27 @@ package com.company.Lesson12;
 import com.company.Lesson9HomeWork.Test;
 
 public class Test10 {
-    int a = 5;
-    void maximum(int i1,int i2,int i3){
-        if(i1>i2 && i1>i3){
-            int a = 10;
-            System.out.println("Maximum - " + i1);
+
+    void abc() {
+        String str;
+        int a = 10;
+        if (a >= 10) {
+            str = "Privet";
         }
-        else if(i2>i1 && i2>i3){
-            int a = 15;
-            System.out.println("Maximum - " + i2);
+        if (a < 10) {
+            str = "Poka";
+        } else {
+            str = null;
         }
-        else{
-            System.out.println("Maximum - " + i3);
-        }
+        System.out.println(str);
     }
 
     public static void main(String[] args) {
-        Test10 t = new Test10();
-        t.maximum(4,7,0);
+        int a = 10;
+        int b = 20;
+        int maximum = (a>b)?a:b;
+        System.out.println(maximum);
+
 
     }
 }
