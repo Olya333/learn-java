bee48d816c8001949688042baf65d53dfc434921
Olya <kandikowa.ol@yandex.ru>
2020-11-30 10:33:32 +0300

    пример как сравнить числа с использованием if

--- src/com/company/Lesson12/Test10.java
+++ src/com/company/Lesson12/Test10.java
@@ -1,17 +1,26 @@
 package com.company.Lesson12;
 
-public class Test10 {
-    public static void main(String[] args) {
-        int salary = 300;
+import com.company.Lesson9HomeWork.Test;
 
-        if (salary < 200) {
-            System.out.println("Z/p ochen nizka");
-        } else if (salary < 400) {
-            System.out.println("Z/p srednego razmera");
-        } else if (salary < 600) {
-            System.out.println("Z/p visokaya");
-        } else {
-            System.out.println("Z/p otlichnaya");
+public class Test10 {
+    int a = 5;
+    void maximum(int i1,int i2,int i3){
+        if(i1>i2 && i1>i3){
+            int a = 10;
+            System.out.println("Maximum - " + i1);
         }
+        else if(i2>i1 && i2>i3){
+            int a = 15;
+            System.out.println("Maximum - " + i2);
+        }
+        else{
+            System.out.println("Maximum - " + i3);
+        }
+    }
+
+    public static void main(String[] args) {
+        Test10 t = new Test10();
+        t.maximum(4,7,0);
+
     }
 }
