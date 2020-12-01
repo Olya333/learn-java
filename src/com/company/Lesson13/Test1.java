d439e2a6ab739774da4e7fb8a0599bf1d1016de1
Olya <kandikowa.ol@yandex.ru>
2020-12-01 19:13:35 +0300

    switch

--- src/com/company/Lesson13/Test1.java
+++ src/com/company/Lesson13/Test1.java
@@ -4,29 +4,35 @@ public class Test1 {
 
 }
 
-class Student{
-    double grade;
-    Student(double grade){
+class Student {
+    int grade;
+
+    Student(int grade) {
         this.grade = grade;
     }
 
     public static void main(String[] args) {
-        Student st1 = new Student(3);
-        if(st1.grade == 2){
-            System.out.println("Student dvoichnik");
+        Student st1 = new Student(4);
+
+        switch (st1.grade) {
+            case 2:
+                System.out.println("Student dvoishnik");
+                break;
+            case 3:
+                System.out.println("Student troishnik");
+                break;
+            case 4:
+                System.out.println("Student horoshist");
+                break;
+            case 5:
+                System.out.println("Student otlichnik");
+                break;
+            default:
+                System.out.println("ocenka ne verna");
+
+
         }
-        else if(st1.grade ==3){
-            System.out.println("Student troishnik");
-        }
-        else if(st1.grade ==4){
-            System.out.println("Student horoshist");
-        }
-        else if(st1.grade ==5){
-            System.out.println("Student otlichnik");
-        }
-        else{
-            System.out.println("ocenka ne verna");
-        }
+
     }
 
 }
