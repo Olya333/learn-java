d8a3bf9d618390a0e09c8df48d8bf52abfac75d0
Olya <kandikowa.ol@yandex.ru>
2020-12-05 15:30:22 +0300

    пример что в теле ащк можно вызвать метод abc

--- src/com/company/Lesson14/Test2.java
+++ src/com/company/Lesson14/Test2.java
@@ -3,8 +3,8 @@ package com.company.Lesson14;
 public class Test2 {
     public static void main(String[] args) {
 
-        for ( int i = 5, j=10;i < 11 && i> -3 || i>4 ; i++,j=j+20 ,abc(10)) {
-            System.out.println(i+j);
+        for ( int i = 5;i < 11 ; i++,abc(10)) {
+            System.out.println(i);
         }
     }
     static void abc(int n){
