f188419dc24df7bb408a08617b2d1899a05fed9a
Olya <kandikowa.ol@yandex.ru>
2020-12-05 16:15:53 +0300

    пример 2

--- src/com/company/Lesson14/Test4.java
+++ src/com/company/Lesson14/Test4.java
@@ -2,10 +2,13 @@ package com.company.Lesson14;
 
 public class Test4 {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 15;
+
         if (a > 10) {
-            System.out.println(a);
-            a = a + 10;
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
         }
+
     }
 }
