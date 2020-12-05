08a3eaceb73ad9a044f08ba93007f6a7c076cd9f
Olya <kandikowa.ol@yandex.ru>
2020-12-05 16:04:32 +0300

    if(false)исключение из правил

--- src/com/company/Lesson14/Test2.java
+++ src/com/company/Lesson14/Test2.java
@@ -3,11 +3,10 @@ package com.company.Lesson14;
 public class Test2 {
     public static void main(String[] args) {
 
-        for ( int i = 5;i < 11 ; i++,abc(10)) {
+        for ( int i = 5;i < 11 ; i++) {
             System.out.println(i);
         }
     }
-    static void abc(int n){
-        System.out.println(n);
-    }
+
+
 }
