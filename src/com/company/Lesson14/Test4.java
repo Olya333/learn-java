67f9eead01b85f7d987a1d33d5999cb54c21d73e
Olya <kandikowa.ol@yandex.ru>
2020-12-05 16:10:40 +0300

    пример 1

---
+++ src/com/company/Lesson14/Test4.java
@@ -0,0 +1,11 @@
+package com.company.Lesson14;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int a = 5;
+        if (a > 10) {
+            System.out.println(a);
+            a = a + 10;
+        }
+    }
+}
