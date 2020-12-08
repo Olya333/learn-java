415573c9578237ee39a7feca44ba5f62b0013d80
Olya <kandikowa.ol@yandex.ru>
2020-12-08 20:37:42 +0300

    примеры как использовать do while

---
+++ src/com/company/Lesson15/Test7.java
@@ -0,0 +1,13 @@
+package com.company.Lesson15;
+
+public class Test7 {
+    public static void main(String[] args) {
+        int money = 0;
+        do{
+            System.out.println("delaite stavki");
+            System.out.println("vi proigrali");
+            money-=10;
+        }
+        while(money>50);
+    }
+}
---
+++ src/com/company/Lesson15/Test8.java
@@ -0,0 +1,10 @@
+package com.company.Lesson15;
+
+public class Test8 {
+    public static void main(String[] args) {
+        while(10==10){
+            System.out.println("privet");
+        }
+
+    }
+}
