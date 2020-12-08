3c3e8fe319c26a6eaaf834b6128e5b6dc39ad71d
Olya <kandikowa.ol@yandex.ru>
2020-12-08 18:41:45 +0300

    пример 2 как использовать while

---
+++ src/com/company/Lesson15/Test2.java
@@ -0,0 +1,13 @@
+package com.company.Lesson15;
+
+public class Test2 {
+    public static void main(String[] args) {
+        int money=100;
+        while(money>0){
+            System.out.println("Delaite stavku");
+            System.out.println("Vi proigrali");
+            money = money - 10;
+        }
+
+    }
+}
