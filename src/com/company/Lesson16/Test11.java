3fc812c8725eeeadce036a297e51b0f4641d48c4
Olya <kandikowa.ol@yandex.ru>
2020-12-12 17:50:47 +0300

    String дополнения

---
+++ src/com/company/Lesson16/Test11.java
@@ -0,0 +1,19 @@
+package com.company.Lesson16;
+
+public class Test11 {
+    public static void main(String[] args) {
+       // String s1= "privet";
+       // System.out.println(s1.isBlank());
+
+        //String s2 = "      ";
+        //System.out.println(s2.isBlank());
+
+        String s3 ="";
+        System.out.println(s3.isEmpty());
+
+        String s4 = "     privet    ";
+       // System.out.println(s4.stripLeading());
+
+
+    }
+}
