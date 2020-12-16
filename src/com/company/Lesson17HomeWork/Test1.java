01b6f8826136d7ad0e1396a25e3f889a17b7875c
Olya <kandikowa.ol@yandex.ru>
2020-12-16 21:06:46 +0300

    дз лекция 17

--- src/com/company/Lesson17HomeWork/Test1.java
+++ src/com/company/Lesson17HomeWork/Test1.java
@@ -1,8 +1,34 @@
 package com.company.Lesson17HomeWork;
 
 public class Test1 {
-    public void ravenstvo(StringBuilder sb1,StringBuilder sb2){
 
+    public static boolean ravenstvo(StringBuilder sb1, StringBuilder sb2) {
+        boolean result = true;
+        if (sb1.length() == sb2.length()) {
+            for (int i = 0; i < sb1.length(); i++) {
+                if (sb1.charAt(i) != sb2.charAt(i)) {
+                    result = false;
+                    break;
+                }
+            }
+        } else {
+            result = false;
+        }
+        return result;
 
+    }
+
+}
+
+class Test1Test{
+    public static void main(String[] args) {
+        StringBuilder sb3 = new StringBuilder("hello");
+        StringBuilder sb4 = new StringBuilder("hello");
+        StringBuilder sb5 = new StringBuilder("Hello");
+        boolean a = Test1.ravenstvo(sb3,sb5);
+        System.out.println(a);
+        System.out.println(Test1.ravenstvo(sb4,sb5));
+        System.out.println(Test1.ravenstvo(new StringBuilder(""),new StringBuilder("")));
+        System.out.println(Test1.ravenstvo(new StringBuilder(),new StringBuilder()));
     }
 }
