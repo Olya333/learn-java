1c85d9ff84224405c847ad45995b83336d589eb0
Olya <kandikowa.ol@yandex.ru>
2020-12-20 14:43:14 +0300

    исключение

---
+++ src/com/company/Lesson18/Test3.java
@@ -0,0 +1,12 @@
+package com.company.Lesson18;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int[]array= new int[3];
+        array[0]=1;
+        array[1]=2;
+        array[0]=3;
+
+        int[][]array2 = new int[3][];
+    }
+}
