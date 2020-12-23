35ff23f383b92f7c20879a108a816f3d7eac4d2c
Olya <kandikowa.ol@yandex.ru>
2020-12-23 11:57:15 +0300

    пример foreach

---
+++ src/com/company/Lesson19/Test3.java
@@ -0,0 +1,16 @@
+package com.company.Lesson19;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int [] array = {0,6,4,1};
+       // for(int i = 0;i< array.length;i++){
+       //     System.out.print(array[i]+ " ");
+       // }
+      //  System.out.println();
+
+        for(int a:array){
+            System.out.print(a+" ");
+        }
+        System.out.println();
+    }
+}
