a194bdb586402e49655fa66da9f42285d4d78ea1
Olya <kandikowa.ol@yandex.ru>
2020-12-24 19:05:45 +0300

    тема Command line arguments

---
+++ src/com/company/Lesson19/Test1.java
@@ -0,0 +1,22 @@
+package com.company.Lesson19;
+
+public class Test1 {
+    static void summa(String s,int... a) {
+        int summa = 0;
+        for (int i = 0; i < a.length; i++) {
+            summa += a[i];
+        }
+        System.out.println(summa);
+        System.out.println(s);
+    }
+
+    public void abc(int[]...array){
+
+    }
+
+    public static void main(String[] args) {
+
+        summa("ok",6,3,10);
+    }
+}
+
---
+++ src/com/company/Lesson19/Test2.java
@@ -0,0 +1,8 @@
+package com.company.Lesson19;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Nulevoy element masssiva; "+ args[0]);
+        System.out.println("Dlinna massiva;" + args.length);
+    }
+}
