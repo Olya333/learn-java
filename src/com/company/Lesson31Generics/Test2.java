40a5326b944cd02b80a2755dcc019e018feb5f2a
Olya <kandikowa.ol@yandex.ru>
2021-04-26 15:25:03 +0300

    Дженерики

---
+++ src/com/company/Lesson31Generics/Test2.java
@@ -0,0 +1,26 @@
+package com.company.Lesson31Generics;
+
+import java.util.ArrayList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList <Integer> al1 = new ArrayList<>();
+        al1.add(10);
+        al1.add(30);
+        al1.add(150);
+        ArrayList <String> al2 = new ArrayList<>();
+        al2.add("a");
+        al2.add("b");
+        al2.add("c");
+        int a = GenMethod.getSecondElement(al1);
+        String s = GenMethod.getSecondElement(al2);
+        System.out.println(s);
+    }
+
+}
+
+class GenMethod{
+    public static <T> T getSecondElement(ArrayList <T> al){
+        return al.get(1);
+    }
+}
